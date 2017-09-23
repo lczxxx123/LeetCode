33219d4b8599abe5e1e9037533bbb40915ad3c2d
lczxxx123 <lczxxx123@users.noreply.github.com>
2017-09-23 21:24:36 +0800

    Create 1-Two Sum.py

---
+++ 1-Two Sum.py
@@ -0,0 +1,14 @@
+"""
+看了一下，做法都是hash。
+有的用map函数，有的用dict，java和c也是。
+"""
+class Solution(object):
+    def twoSum(self, nums, target):
+        if len(nums) <= 1:
+            return False
+        buff_dict = {}
+        for i in range(len(nums)):
+            if nums[i] in buff_dict:
+                return [buff_dict[nums[i]], i]
+            else:
+                buff_dict[target - nums[i]] = i
